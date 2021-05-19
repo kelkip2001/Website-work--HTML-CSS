4bbb6b143903165ccf39a5041474054e3840ee5f
kelvin <kelkip2001@gmail.com>
2021-05-19 21:48:25 +0300

    finished website for ecommerce - front end

---
+++ College website/form-handler.php
@@ -0,0 +1,22 @@
+<?php
+$name=$_POST['name'];
+$visitor_email =$_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@ibima.or.ke';
+$email_subject = 'New Form Submission';
+
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"subject: $subject.\n". 
+"User Message: $message.\n". 
+
+$to ='kelkip2001@gmail.com';
+$headers = "From: $email_from \r\n";
+$header .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("location:contact.html")
+?>
